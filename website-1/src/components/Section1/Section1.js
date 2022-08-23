3dd8f0c6446036c0e52c0011d6baaa6ecde2de1a
dipanwitaxd <dipanwita678@gmail.com>
2022-08-24 00:30:01 +0530

    added navbar

--- website-1/src/components/Section1/Section1.js
+++ website-1/src/components/Section1/Section1.js
@@ -3,11 +3,10 @@ import "./Section1.css";
 import { useState, useEffect } from "react";
 import logo from "../../assets/workskill-proj-1-img-2.svg";
 
-
 function Section1() {
-    const [header, setHeader] = useState("header");
+  const [header, setHeader] = useState("header");
 
-  const listenScrollEvent = event => {
+  const listenScrollEvent = (event) => {
     if (window.scrollY < 73) {
       return setHeader("header");
     } else if (window.scrollY > 70) {
@@ -23,23 +22,26 @@ function Section1() {
 
   return (
     <div>
-        <div className={header}>
-      <div className="navbar-sec">
-        <div className="nav-left">
-          <img src={logo} alt="logo" />
-          <div>Support</div>
-          <div>Product</div>
-          <div>Company</div>
-          <div>Account</div>
+      <div className={header}>
+        <div className="navbar-sec">
+          <div className="nav-left">
+            <img src={logo} alt="logo" />
+            <div>Support</div>
+            <div>Product</div>
+            <div>Company</div>
+            <div>Account</div>
+          </div>
+          <div className="nav-right">
+            <button className="nav-right-button">Buy Template</button>
+          </div>
         </div>
-        <div className="nav-right">
-          <button className="nav-right-button">Buy Template</button>
-        </div>
-      </div>
       </div>
       <div className="section1-div">
         <div className="section1-text">
-          <div className="text-sec"> Build like the best teams on the planet</div>
+          <div className="text-sec">
+            {" "}
+            Build like the best teams on the planet
+          </div>
         </div>
       </div>
     </div>
