05958e0cf97d8701327092a84bd51534f38e201f
dipanwitaxd <dipanwita678@gmail.com>
2022-08-24 00:26:46 +0530

    nav bar transition

--- website-1/src/components/Section1/Section1.js
+++ website-1/src/components/Section1/Section1.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Section1.css";
+import { useState, useEffect } from "react";
 import logo from "../../assets/workskill-proj-1-img-2.svg";
 
+
 function Section1() {
+    const [header, setHeader] = useState("header");
+
+  const listenScrollEvent = event => {
+    if (window.scrollY < 73) {
+      return setHeader("header");
+    } else if (window.scrollY > 70) {
+      return setHeader("header2");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+
+    return () => window.removeEventListener("scroll", listenScrollEvent);
+  }, []);
+
   return (
     <div>
+        <div className={header}>
       <div className="navbar-sec">
         <div className="nav-left">
           <img src={logo} alt="logo" />
@@ -16,6 +35,7 @@ function Section1() {
         <div className="nav-right">
           <button className="nav-right-button">Buy Template</button>
         </div>
+      </div>
       </div>
       <div className="section1-div">
         <div className="section1-text">
