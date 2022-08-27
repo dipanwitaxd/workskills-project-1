febbf43982ee6bded1064f01fd5d7a48f8a2f080
dipanwitaxd <dipanwita678@gmail.com>
2022-08-27 11:39:49 +0530

    section 4

--- website-1/src/App.js
+++ website-1/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Section4 from './components/Section4/Section4';
 import Section1 from './components/Section1/Section1';
 import Section2 from './components/Section2/Section2';
 import Section3 from './components/Section3/Section3';
@@ -13,6 +14,7 @@ function App() {
       </div>   
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
---
+++ website-1/src/components/Section4/Section4.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Section4.css";
+import image1 from "../../assets/workskill-proj-1-img-8.png";
+import FingerprintRounded from "@mui/icons-material/FingerprintRounded";
+import DoneRounded from "@mui/icons-material/DoneRounded";
+
+function Section4() {
+  return (
+    <div className="section4-background">
+      <div className="image-background">
+        <img src={image1} alt="img" className="img"></img>
+      </div>
+      <div className="sec4-col">
+        <div className="section4-col2">
+          <div className="fingerprint">
+            {" "}
+            <FingerprintRounded />{" "}
+          </div>
+          <div>
+            <div className="heading-sec4">
+              Top notch perks for top notch{" "}
+              <span className="gradient-head">people</span>.
+            </div>
+            <div className="sub-text">
+              Stand out online with a professional website, online store, or
+              portfolio. With Fouroom templates, you can turn any idea into a
+              reality.
+            </div>
+          </div>
+        </div>
+        <div className="row2-sec4">
+          <div
+            className="border-line"
+            data-aos="fade-up"
+            
+          ></div>
+          <div className="line" data-aos="flip-up" >
+            <div className="lines">Add and edit schema markup</div>
+            <DoneRounded
+              style={{ color: "rgb(167,181,196)", fontSize: ".875em" }}
+            />
+          </div>
+          <div></div>
+          <div
+            className="border-line"
+            data-aos="fade-up"
+            
+          ></div>
+          <div className="line" data-aos="fade-up" >
+            <div className="lines">Meta titles and meta descriptions</div>
+            <div className="lines">Unlimited</div>
+          </div>
+          <div></div>
+          <div className="border-line" data-aos="fade-up"></div>
+          <div className="line" data-aos="fade-up" >
+            <div className="lines">Indexing and sitemap controls</div>
+            <div className="lines" >Easy edit</div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Section4;
